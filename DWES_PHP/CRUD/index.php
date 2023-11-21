2836ab9dec2f0a7ff5ab56f9745ab65917404e8f
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2023-11-21 12:25:16 +0100

    Filtro por precio

--- DWES_PHP/CRUD/index.php
+++ DWES_PHP/CRUD/index.php
@@ -37,9 +37,7 @@ listarPizzas($conn);
 
 
 
-//iNSERTAR NUEVA PIIZA
-
-
+//INSERTAR NUEVA PIZA
 echo "<h1>Nuestas Pizzas</h1>";
 listarPizzas($conn);
 $nombrePizza = "Pizza prueba";
@@ -64,10 +62,63 @@ $insertar = $conn -> prepare("INSERT INTO pizza (nombre, coste, precio, ingredie
 $insertar->execute();
 
 
+//Modificar pizza
+echo "<h1>Modificar Pizza</h1>";
+
+$nombrePizza = "Pizza modificada";
+$costePizzas = 6.00;
+$precioPizza = 15.00;
+$ingredientesPizza = "Tomate, Mozzarella, Champiñones";
+$nombreOriginal = "Pizza prueba";
+
+$modificar = $conn->prepare("UPDATE  pizza SET nombre = :nombre,
+ coste = :coste, ingredientes = :ingredientes, 
+ precio = :precio WHERE nombre = :nombreOriginal");
+
+$modificar->bindParam(':nombre', $nombrePizza);
+$modificar->bindParam(':coste', $costePizzas);
+$modificar->bindParam(':precio', $precioPizza);
+$modificar->bindParam(':ingredientes', $ingredientesPizza);
+$modificar->bindParam(':nombreOriginal', $nombreOriginal);
+
+$modificar->execute();
+
+listarPizzas($conn);
 
 
 
+//Metodo para borrar ciertas pizzas
+echo "<h1>BORRAR PIZZA MODIFICADA</h1>";
+
+$nombreBorrar = "Pizza Modificada";
+$eliminar = $conn->prepare("DELETE FROM pizza WHERE nombre = :nombre ");
+$eliminar ->bindParam(':nombre', $nombreBorrar);
+$eliminar -> execute();
+
+echo "<h2>Pizzas tras borrar</h2>";
+
+listarPizzas($conn);
 
 
+//Metodo para filtrar por campo precio
+
+echo "<h1>Filtro por precio de 10€</h1>";
+
+function pizzaPrecio($conn, $precioFiltro){
+    $consulta = $conn->prepare("SELECT nombre,precio FROM pizza WHERE precio = :precio");
+    
+    //Bindeamos el parametro
+    $consulta ->bindParam(':precio', $precioFiltro);
+
+    //la ejecutamos
+    $consulta->execute();
+
+    //La imprimimos por pantalla
+    foreach ($consulta->fetchAll(PDO::FETCH_ASSOC) as $row) {
+        echo $row["nombre"] . "." . $row["precio"] . "€.<br>";
+    }
+};
+echo "<h2>Pizzas con precio igual a 10.00 euros</h2>";
+pizzaPrecio($conn, 10.00);
 
 ?>
